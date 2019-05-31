e66e99eabd39c03a5533acf98adc2a3775bcfaa4
Noordeen Malango <noordinmalango@gmail.com>
2019-05-31 14:45:47 +0200

    testing on my all , none, count map and  inject methods

--- lib/Enumerable_methods.rb
+++ lib/Enumerable_methods.rb
@@ -6,7 +6,7 @@ module Enumerable
       k += 1
     end
     self
-  end 
+  end
 
   def my_each_with_index
     l = self.length-1
@@ -14,17 +14,17 @@ module Enumerable
       yield(self[k], k)
     end
     self
-  end 
+  end
 
   def my_select
     arr = []
-    self.my_each do |k| 
+    self.my_each do |k|
       if yield(k)
         arr.push(k)
       end
     end
     arr
-  end 
+  end
 
   def my_all?
     k = 0
@@ -35,7 +35,7 @@ module Enumerable
       k += 1
     end
     true
-  end 
+  end
 
   def my_any?
     k = 0
@@ -46,7 +46,7 @@ module Enumerable
       k += 1
     end
     false
-  end 
+  end
 
   def my_none?
     k = 0
@@ -73,33 +73,19 @@ module Enumerable
       k += 1
     end
     c
-  end 
+  end
 
   def my_map
     new_array = []
     self.my_each{|element| new_array << element if yield(element)}
     new_array
-  end 
+  end
 
   def my_inject(v=nil)
-    v=0 if v.nil? 
+    v=0 if v.nil?
     self.my_each do |k|
       v = yield(v, k)
     end
     v
   end
 end
-
-def multiply_els(ar)
-  ar.my_inject(1) { |sum, k| sum * k }
-end
-
-#[1,2,3].my_each{|i| puts i}
-#[1,2,3].my_each_with_index{|i, value| puts "#{i} #{value}"}
-#puts [1,2,3].my_select{|i|  i>=2}
-#puts [1,2,3].my_all?{|i|  i>2}
-#puts [1,2,3].my_any?{|i| i>4}
-#puts [1,2,3].my_none?{|i|  i>2}
-#puts [1,2,3,4].my_count{|i| i>=2}
-#puts [1,2,3,4].my_map{|i| i>2}
-puts multiply_els([5,2,3,4])
--- spec/Enumerable_methods_spec.rb
+++ spec/Enumerable_methods_spec.rb
@@ -8,11 +8,11 @@ describe Enumerable do
         before do
             array.my_each {|i| output << i*i}
         end
-        it "returns an array after something is done to the elements of it" do 
-            expect(output).to eql (result)  
+        it "returns an array after something is done to the elements of it" do
+            expect(output).to eql (result)
         end
     end
-    
+
     context "testing my_each_with_index on array" do
         let(:array) {[1,2,3,4,5,6,7]}
         let(:result) {[1,2,3,4,5,6,7]}
@@ -20,26 +20,114 @@ describe Enumerable do
         before do
             array.my_each_with_index {|val, index| output << index+1}
         end
-        it "returns an array of indexes after adding 1 to each index" do 
-            expect(output).to eql (result)  
+        it "returns an array of indexes after adding 1 to each index" do
+            expect(output).to eql (result)
         end
-    end 
+    end
 
     context "testing my_select on array" do
         let(:array) {[1,2,3,4,5,6,7]}
         let(:result) {[2,4,6]}
-        it "returns an array with the elements of the original array which satisfies a certain condition" do 
+        it "returns an array with the elements of the original array which satisfies a certain condition" do
             output = array.my_select {|i| i%2 == 0}
-            expect(output).to eql (result)  
+            expect(output).to eql (result)
         end
     end
 
     context "testing my_all? on array" do
         let(:array) {[1,2,3,4,5,6,7]}
-        let(:result) {[2,4,6]}
-        it "returns an array with the elements of the original array which satisfies a certain condition" do 
-            output = array.my_select {|i| i%2 == 0}
-            expect(output).to eql (result)  
+        let(:result) {true}
+        it "returns true if all numbers are greater than 0" do
+            output = array.my_all? {|i| i>0}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_all? on array" do
+        let(:array) {[1,2,3,4,5,6,7]}
+        let(:result) {false}
+        it "returns false if not all number satisfies a condition" do
+            output = array.my_all? {|i| i%2 == 0}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_all? on array" do
+        let(:array) {["N","M"]}
+        let(:result) {true}
+        it "returns true if  all elements are strings" do
+            output = array.my_all? {|i| i.is_a? (String)}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_any? on array" do
+        let(:array) {[1,2,3,4,5,6,7]}
+        let(:result) {true}
+        it "returns true if there is any element that satisfies the condition" do
+            output = array.my_any? {|i| i%2 == 0}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_any? on array" do
+        let(:array) {[1,2,3,4,5,6,7]}
+        let(:result) {false}
+        it "returns false if there no element that satisfies the condition" do
+            output = array.my_any? {|i| i>7}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_none? on array" do
+        let(:array) {[1,2,3,4,5,6,7]}
+        let(:result) {false}
+        it "returns false if there is an element that satisfies the condition" do
+            output = array.my_none? {|i| i>3}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_none? on array" do
+        let(:array) {[1,2,3,4,5,6,7]}
+        let(:result) {true}
+        it "returns true if there is no element that satisfies the condition" do
+            output = array.my_none? {|i| i>7}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_count on array" do
+        let(:array) {[1,2,3,4,5,6,7]}
+        let(:result) {4}
+        it "returns number of elements in array that satisfies the condition if given" do
+            output = array.my_count {|i| i>3}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_count on array" do
+        let(:array) {[1,2,3,4,5,6,7]}
+        let(:result) {7}
+        it "returns number of elements in array if a block is not given" do
+            output = array.my_count
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_map on array" do
+        let(:array) {[1,2,3,4,5,6,7]}
+        let(:result) {[1,2,3]}
+        it "returns an array of elements that satisfies the condition" do
+            output = array.my_map {|i| i<4}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_inject on array" do
+        let(:array) {[1,2,3,4,5]}
+        let(:result) {15}
+        it "returns the sum of elements without pass a parameter" do
+            output = array.my_inject {|sum, k| sum + k}
+            expect(output).to eql (result)
+        end
+    end
+    context "testing my_inject on array" do
+        let(:array) {[1,2,3,4,5]}
+        let(:result) {120}
+        it "returns the product of elements after passing initial value of product as 1" do
+            output = array.my_inject(1) {|product, k| product * k}
+            expect(output).to eql (result)
         end
     end
 end
